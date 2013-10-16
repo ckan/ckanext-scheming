79af50b0b4cd211b78473c1761a48b88ac443289
Ian Ward <ian@excess.org>
2013-10-16 18:20:39 -0400

    show command

--- ckanext/scheming/commands.py
+++ ckanext/scheming/commands.py
@@ -20,3 +20,31 @@ def _get_schemas():
             dataset_schemas = plugin._schemas
     return (dataset_schemas, group_schemas)
 
+
+class SchemingCommand(CkanCommand):
+    """
+    ckanext-scheming schema management commands
+
+    Usage::
+
+        paster scheming show
+    """
+    summary = __doc__.split('\n')[0]
+    usage = __doc__
+
+    parser = paste.script.command.Command.standard_parser(verbose=True)
+    parser.add_option('-c', '--config', dest='config',
+        default='development.ini', help='Config file to use.')
+
+    def command(self):
+        cmd = self.args[0]
+        self._load_config()
+
+        if cmd == 'show':
+            self._show()
+        else:
+            print self.__doc__
+
+    def _show(self):
+        print _get_schemas()
+
