fa699d5e8ccd0b03847e305dcf403e034f154eb1
Ian Ward <ian@excess.org>
2014-10-01 14:20:39 -0400

    test all get_validator paths

---
+++ ckanext/scheming/tests/test_validation.py
@@ -0,0 +1,15 @@
+from nose.tools import assert_raises
+
+from ckanext.scheming.errors import SchemingException
+from ckanext.scheming.validation import get_validator_or_converter
+
+class TestGetValidatorOrConverter(object):
+    def test_missing(self):
+        assert_raises(SchemingException,
+            get_validator_or_converter, 'not_a_real_validator_name')
+
+    def test_validator_name(self):
+        assert get_validator_or_converter('not_empty')
+
+    def test_converter_name(self):
+        assert get_validator_or_converter('date_to_db')
