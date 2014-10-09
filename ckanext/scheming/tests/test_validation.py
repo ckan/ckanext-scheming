2c279d2836711ccab46b460ea74c7e1bd4ce0849
Ian Ward <ian@excess.org>
2014-10-09 13:17:42 -0400

    failing test first

--- ckanext/scheming/tests/test_validation.py
+++ ckanext/scheming/tests/test_validation.py
@@ -1,4 +1,5 @@
-from nose.tools import assert_raises
+from nose.tools import assert_raises, assert_equals
+from ckanapi import LocalCKAN, ValidationError
 
 from ckanext.scheming.errors import SchemingException
 from ckanext.scheming.validation import get_validator_or_converter
@@ -13,3 +14,12 @@ class TestGetValidatorOrConverter(object):
 
     def test_converter_name(self):
         assert get_validator_or_converter('date_to_db')
+
+class TestChoices(object):
+    def test_choice_field_only_accepts_given_choices(self):
+        lc = LocalCKAN()
+        assert_raises(ValidationError, lc.action.package_create, **{
+            'type':'camel-photos',
+            'name':'fred',
+            'class': 'rocker',
+            })
