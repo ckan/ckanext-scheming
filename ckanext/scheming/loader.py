93af96f2b894278bdda958906956da0d74e397ec
Ian Ward <ian@excess.org>
2015-10-30 17:05:36 -0400

    ImportError on missing pyyaml instead of AttributeError

--- ckanext/scheming/loader.py
+++ ckanext/scheming/loader.py
@@ -4,18 +4,15 @@ Load either yaml or json, based on the name of the resource
 
 import json
 
-try:
-    import yaml
-except ImportError:
-    yaml = None
-
 def load(f):
     if is_yaml(f.name):
+        import yaml
         return yaml.load(f)
     return json.load(f)
 
 def loads(s, url):
     if is_yaml(url):
+        import yaml
         return yaml.load(s)
     return json.loads(s)
 
