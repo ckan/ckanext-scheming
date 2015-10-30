7fc4adefe13e8832e7283c467e75bb6ff468b55d
Tyler Kennedy <tk@tkte.ch>
2015-10-30 15:39:02 -0500

    [feature] Added LoaderError exception, and a helpful error message if attempting to load a YAML file without PyYAML.

--- ckanext/scheming/errors.py
+++ ckanext/scheming/errors.py
@@ -1,4 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+__all__ = (
+    'SchemingException',
+    'LoaderError'
+)
+
 
 class SchemingException(Exception):
     pass
 
+
+class LoaderError(Exception):
+    """
+    Raised when an error occurs while attempting to load a
+    schema.
+    """
--- ckanext/scheming/loader.py
+++ ckanext/scheming/loader.py
@@ -3,21 +3,36 @@ Load either yaml or json, based on the name of the resource
 """
 
 import json
+from functools import partial
 
 try:
     import yaml
 except ImportError:
     yaml = None
 
+from ckanext.scheming.errors import LoaderError
+
+YamlMissingError = partial(
+    LoaderError,
+    'PyYAML dependency is missing, unable to load YAML schema file.'
+)
+
+
 def load(f):
     if is_yaml(f.name):
+        if yaml is None:
+            raise YamlMissingError()
         return yaml.load(f)
     return json.load(f)
 
+
 def loads(s, url):
     if is_yaml(url):
+        if yaml is None:
+            raise YamlMissingError()
         return yaml.load(s)
     return json.loads(s)
+
 
 def is_yaml(n):
     return n[-5:].lower() == '.yaml' or n[-4:] == '.yml'
