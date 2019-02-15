477225ea1c625e2da0f583e181fa1e0c204edc7f
Ian Ward <ian@excess.org>
2019-02-15 13:21:57 -0500

    revert some noise

--- ckanext/scheming/logic.py
+++ ckanext/scheming/logic.py
@@ -6,23 +6,21 @@ from ckanext.scheming.helpers import (
     scheming_organization_schemas, scheming_get_organization_schema,
     )
 
-
 @side_effect_free
 def scheming_dataset_schema_list(context, data_dict):
-    """
+    '''
     Return a list of dataset types customized with the scheming extension
-    """
+    '''
     return list(scheming_dataset_schemas())
-
 
 @side_effect_free
 def scheming_dataset_schema_show(context, data_dict):
-    """
+    '''
     Return the scheming schema for a given dataset type
 
     :param type: the dataset type
     :param expanded: True to expand presets (default)
-    """
+    '''
     t = get_or_bust(data_dict, 'type')
     expanded = data_dict.get('expanded', True)
     s = scheming_get_dataset_schema(t, expanded)
@@ -30,23 +28,21 @@ def scheming_dataset_schema_show(context, data_dict):
         raise ObjectNotFound()
     return s
 
-
 @side_effect_free
 def scheming_group_schema_list(context, data_dict):
-    """
+    '''
     Return a list of group types customized with the scheming extension
-    """
+    '''
     return list(scheming_group_schemas())
-
 
 @side_effect_free
 def scheming_group_schema_show(context, data_dict):
-    """
+    '''
     Return the scheming schema for a given group type
 
     :param type: the group type
     :param expanded: True to expand presets (default)
-    """
+    '''
     t = get_or_bust(data_dict, 'type')
     expanded = data_dict.get('expanded', True)
     s = scheming_get_group_schema(t, expanded)
@@ -57,20 +53,19 @@ def scheming_group_schema_show(context, data_dict):
 
 @side_effect_free
 def scheming_organization_schema_list(context, data_dict):
-    """
+    '''
     Return a list of organization types customized with the scheming extension
-    """
+    '''
     return list(scheming_organization_schemas())
-
 
 @side_effect_free
 def scheming_organization_schema_show(context, data_dict):
-    """
+    '''
     Return the scheming schema for a given organization type
 
     :param type: the organization type
     :param expanded: True to expand presets (default)
-    """
+    '''
     t = get_or_bust(data_dict, 'type')
     expanded = data_dict.get('expanded', True)
     s = scheming_get_organization_schema(t, expanded)
