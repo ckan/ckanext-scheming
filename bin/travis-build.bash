b40ffe11c9d0df88adbf4e52ee6fc16470809151
Ian Ward <ian@excess.org>
2020-04-24 10:14:37 -0400

    tests: update setuptools

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -26,6 +26,10 @@ else
     echo "CKAN version: ${CKAN_TAG#ckan-}"
 fi
 
+if [ -f requirement-setuptools.txt ]
+then
+    pip install -r requirement-setuptools.txt
+fi
 python setup.py develop
 
 if [ -f requirements-py2.txt ] && [ $PYTHONVERSION = 2 ]
