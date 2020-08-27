8a2f7b0478cdba34cefcf9cad461ef1232ad3a8e
maxFredenburgh <maxwellfredenburgh.dev@gmail.com>
2020-08-27 12:53:45 -0400

    separate module for removal popup

--- ckanext/scheming/fanstatic/scheming-repeating-subfields.js
+++ ckanext/scheming/fanstatic/scheming-repeating-subfields.js
@@ -5,8 +5,7 @@ this.ckan.module('scheming-repeating-subfields', function (jQuery, _) {
                 $this = $(this.el),
                 $template = $this.children('div[name="repeating-template"]'),
                 template = $template.html(),
-                $add = $this.children('a[name="repeating-add"]'),
-                $remove = $this.find('a[name="repeating-remove"]');
+                $add = $this.children('a[name="repeating-add"]');
             $template.remove();
 
             $add.on('click', function(e) {
@@ -18,25 +17,6 @@ this.ckan.module('scheming-repeating-subfields', function (jQuery, _) {
                 $copy.insertAfter($last);
                 // hook for late init when required for rendering polyfills
                 $this.trigger('scheming.subfield-group-init');
-                e.preventDefault();
-            });
-
-            $remove.on('click', function(e) {
-                var $groups = $this.find('.scheming-subfield-group'),
-                    $curr = $(this).closest('.scheming-subfield-group'),
-                    index = $curr.data('groupIndex'),
-                    count = $groups.length;
-                document.getElementById("statusText1").innerHTML = (index+1)+"?";
-                document.getElementById("statusText0").style.display = 'inline';
-                document.getElementById("statusText1").style.display = 'inline';
-                $("#confirmRemoval").on('click', function(e) {
-                    if(count ==1){
-                        $add.click();
-                        count +=1;
-                    }
-                    $curr.remove();
-                });
-
                 e.preventDefault();
             });
         }
