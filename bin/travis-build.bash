dc15ff78f1e4e41ccdb4dd639b1e8d25a2df31ce
amercader <amercadero@gmail.com>
2020-09-10 16:47:57 +0200

    dev -> test

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -76,7 +76,7 @@ cd -
 
 echo "Installing ckanext-harvest and its requirements..."
 pip install -r requirements.txt
-pip install -r dev-requirements.txt
+pip install -r test-requirements.txt
 
 python setup.py develop
 
