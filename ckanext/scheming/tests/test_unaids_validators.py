f0e9e8e2c3595413638c92104a310e5b0499c59d
Tomasz Sabała <sabala.tomek@gmail.com>
2020-12-07 18:29:32 +0100

    ADX-547 auto_create_valid_name preserves names of existing datasets.

---
+++ ckanext/scheming/tests/test_unaids_validators.py
@@ -0,0 +1,28 @@
+import pytest
+from ckanapi import LocalCKAN
+
+
+@pytest.mark.usefixtures(u'clean_db')
+@pytest.mark.usefixtures(u'clean_index')
+class TestAutoCreateValidName(object):
+    def test_prevents_duplicates(self):
+        lc = LocalCKAN()
+        dataset1, dataset2 = [lc.action.package_create(
+            type="auto-create-valid-name", year="2020", location="north-pole"
+        ) for i in range(2)]
+
+        assert dataset1['name'] == u'north-pole-autogenerate-2020'
+        assert dataset2['name'] == u'north-pole-autogenerate-2020-1'
+
+    def test_preserves_existing_dataset_name(self):
+        lc = LocalCKAN()
+        dataset1 = lc.action.package_create(
+            type="auto-create-valid-name", year="2020", location="north-pole"
+        )
+        dataset2 = lc.action.package_create(
+            type="auto-create-valid-name", year="2020", location="north-pole"
+        )
+        lc.action.package_delete(id=dataset1['id'])
+
+        updated_dataset2 = lc.action.package_update(**dataset2)
+        assert updated_dataset2['name'] == updated_dataset2['name']
