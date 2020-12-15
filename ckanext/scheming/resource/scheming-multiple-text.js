ede52e13f21a9d42cf7616bd8dd170e288b62578
Ian Ward <ian@excess.org>
2020-12-14 19:15:07 -0500

    js, style for multiple_text

---
+++ ckanext/scheming/resource/scheming-multiple-text.js
@@ -0,0 +1,27 @@
+this.ckan.module('scheming-multiple-text', function($, _) {
+  return {
+    initialize: function() {
+      var container = this;
+      var $this = $(this.el);
+      var $template = $this.children('div[name="multiple-template"]');
+      var template = $template.html();
+      var $add = $this.find('a[name="multiple-add"]');
+      $template.remove();
+
+      $add.on('click', function(e) {
+        var $last = $this.find('.multiple-text-field').last();
+        var $copy = $(template);
+        $copy.insertAfter($last).hide().show(100);
+        e.preventDefault();
+      });
+
+      $(document).on('click', 'a[name="multiple-remove"]', function(e) {
+        var $curr = $(this).closest('.multiple-text-field');
+        $curr.hide(100, function() {
+          $curr.remove();
+        });
+        e.preventDefault();
+      });
+    }
+  };
+});
