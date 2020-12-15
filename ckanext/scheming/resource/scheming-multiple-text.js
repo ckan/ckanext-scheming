9cec8455711dd2fedff788a37716623a876fbd47
Ian Ward <ian@excess.org>
2020-12-15 12:28:13 -0500

    multiple-text: fix add button when added in subfields

--- ckanext/scheming/resource/scheming-multiple-text.js
+++ ckanext/scheming/resource/scheming-multiple-text.js
@@ -1,3 +1,4 @@
+var scheming_multiple_text_init_done = false;
 this.ckan.module('scheming-multiple-text', function($, _) {
   return {
     initialize: function() {
@@ -5,24 +6,26 @@ this.ckan.module('scheming-multiple-text', function($, _) {
       var $this = $(this.el);
       var $template = $this.children('div[name="multiple-template"]');
       var template = $template.html();
-      var $add = $this.find('a[name="multiple-add"]');
       $template.remove();
 
-      $add.on('click', function(e) {
-        var $copy = $(template);
-        $this.find('.multiple-text-group').append($copy);
-        $copy.hide().show(100);
-        $copy.find('input').focus();
-        e.preventDefault();
-      });
-
-      $(document).on('click', 'a[name="multiple-remove"]', function(e) {
-        var $curr = $(this).closest('.multiple-text-field');
-        $curr.hide(100, function() {
-          $curr.remove();
+      if (!scheming_multiple_text_init_done) {
+        $(document).on('click', 'a[name="multiple-add"]', function(e) {
+          var $copy = $(template);
+          $(this).closest('.scheming-fieldset').find('.multiple-text-group').append($copy);
+          $copy.hide().show(100);
+          $copy.find('input').focus();
+          e.preventDefault();
         });
-        e.preventDefault();
-      });
+
+        $(document).on('click', 'a[name="multiple-remove"]', function(e) {
+          var $curr = $(this).closest('.multiple-text-field');
+          $curr.hide(100, function() {
+            $curr.remove();
+          });
+          e.preventDefault();
+        });
+        scheming_multiple_text_init_done = true;
+      }
     }
   };
 });
