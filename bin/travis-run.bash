54b60492816df2f14ced8fc3477068f30d560d8d
Ian Ward <ian@excess.org>
2021-01-13 14:33:47 -0500

    github actions CI based on ckanext-harvest

--- bin/travis-run.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-pytest --ckan-ini=subdir/test.ini --cov=ckanext.scheming ckanext/scheming/tests
-pytest --ckan-ini=subdir/test_subclass.ini ckanext/scheming/tests/test_dataset_display.py ckanext/scheming/tests/test_form.py::TestDatasetFormNew ckanext/scheming/tests/test_dataset_logic.py
