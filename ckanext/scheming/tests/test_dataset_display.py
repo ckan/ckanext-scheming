89255d9f8bfb246818ea72e69f91e51757482be4
manoj-nathwani <manoj_1993@hotmail.com>
2021-05-07 12:35:15 +0000

    working tests

--- ckanext/scheming/tests/test_dataset_display.py
+++ ckanext/scheming/tests/test_dataset_display.py
@@ -1,139 +1,180 @@
 import pytest
 import six
-from ckantoolkit.tests.factories import Sysadmin, Dataset, Resource
+from ckan.lib.helpers import url_for
+from ckantoolkit.tests.factories import Sysadmin, User, Dataset
 from bs4 import BeautifulSoup
 
 
 @pytest.mark.usefixtures("clean_db")
 class TestDatasetDisplay(object):
-    def test_dataset_view_core_resources(self, app):
+
+    core_resource = {
+        'name': 'Core Resource',
+        'url': 'http://example.com/camel.txt',
+        'resource_type': 'awesome-resource'
+    }
+    extra_resource = {
+        'name': 'Extra Resource',
+        'url': 'http://example.com/camel.txt',
+    }
+
+    def test_core_resources(self, app):
+        user = User()
         dataset = Dataset(
+            user=user,
             type='test-schema',
-            name='test',
+            resources=[self.core_resource],
+        )
+        response = app.get(
+            url_for('dataset.read', id=dataset['name']),
+            extra_environ={'REMOTE_USER': six.ensure_str(user['name'])},
+        )
+        core_resources_container = BeautifulSoup(response.body)\
+            .select('.core-resources')[0]
+        assert self.core_resource['name'] in str(core_resources_container)
+
+    def test_extra_resources(self, app):
+        user = User()
+        dataset = Dataset(
+            user=user,
+            type='test-schema',
+            resources=[self.extra_resource],
+        )
+        response = app.get(
+            url_for('dataset.read', id=dataset['name']),
+            extra_environ={'REMOTE_USER': six.ensure_str(user['name'])},
+        )
+        extra_resources_container = BeautifulSoup(response.body)\
+            .select('.extra-resources')[0]
+        assert self.extra_resource['name'] in str(extra_resources_container)
+
+    def test_having_both_core_and_extra_resources(self, app):
+        user = User()
+        dataset = Dataset(
+            user=user,
+            type='test-schema',
+            resources=[self.core_resource, self.extra_resource],
+        )
+        response = app.get(
+            url_for('dataset.read', id=dataset['name']),
+            extra_environ={'REMOTE_USER': six.ensure_str(user['name'])},
+        )
+        core_resources_container = BeautifulSoup(response.body)\
+            .select('.core-resources')[0]
+        extra_resources_container = BeautifulSoup(response.body)\
+            .select('.extra-resources')[0]
+        assert self.core_resource['name'] in str(core_resources_container)
+        assert self.core_resource['name'] not in str(extra_resources_container)
+        assert self.extra_resource['name'] in str(extra_resources_container)
+        assert self.extra_resource['name'] not in str(core_resources_container)
+
+    def test_dataset_displays_custom_fields(self, app):
+        user = Sysadmin()
+        Dataset(
+            user=user,
+            type="test-schema",
+            name="set-one",
+            humps=3,
+            resources=[
+                {"url": "http://example.com/camel.txt", "camels_in_photo": 2}
+            ],
+        )
+
+        response = app.get("/dataset/set-one")
+        assert "Humps" in response.body
+
+    def test_resource_displays_custom_fields(self, app):
+        user = Sysadmin()
+        d = Dataset(
+            user=user,
+            type="test-schema",
+            name="set-two",
+            humps=3,
             resources=[
                 {
-                    'url': 'http://example.com/camel.txt',
-                    'resource_type': 'inputs-unaids-geographic'
+                    "url": "http://example.com/camel.txt",
+                    "camels_in_photo": 2,
+                    "date": "2015-01-01",
                 }
             ],
         )
-        Resource(
-            package_id=dataset['id'],
-            resource_type='inputs-unaids-geographic'
+
+        response = app.get(
+            "/dataset/set-two/resource/" + d["resources"][0]["id"]
         )
-        response = app.get('/{}/{}'.format(dataset['type'], dataset['name']))
-        assert 'Geographic Data' in response.body
-        core_resources_container = BeautifulSoup(response.body)\
-            .select('.core-resources')[0]
-        assert dataset['name'] in core_resources_container
+        assert "Camels in Photo" in response.body
+        assert "Date" in response.body
 
-    # def test_dataset_displays_custom_fields(self, app):
-    #     user = Sysadmin()
-    #     Dataset(
-    #         user=user,
-    #         type="test-schema",
-    #         name="set-one",
-    #         humps=3,
-    #         resources=[
-    #             {"url": "http://example.com/camel.txt", "camels_in_photo": 2}
-    #         ],
-    #     )
+    def test_choice_field_shows_labels(self, app):
+        user = Sysadmin()
+        Dataset(
+            user=user,
+            type="test-schema",
+            name="with-choice",
+            category="hybrid",
+        )
+        response = app.get("/dataset/with-choice")
+        assert "Hybrid Camel" in response.body
 
-    #     response = app.get("/dataset/set-one")
-    #     assert "Humps" in response.body
+    def test_notes_field_displayed(self, app):
+        user = Sysadmin()
+        Dataset(
+            user=user,
+            type="dataset",
+            name="plain-jane",
+            notes="# styled notes",
+        )
 
-    # def test_resource_displays_custom_fields(self, app):
-    #     user = Sysadmin()
-    #     d = Dataset(
-    #         user=user,
-    #         type="test-schema",
-    #         name="set-two",
-    #         humps=3,
-    #         resources=[
-    #             {
-    #                 "url": "http://example.com/camel.txt",
-    #                 "camels_in_photo": 2,
-    #                 "date": "2015-01-01",
-    #             }
-    #         ],
-    #     )
+        response = app.get("/dataset/plain-jane")
+        assert "<h1>styled notes" in response.body
 
-    #     response = app.get(
-    #         "/dataset/set-two/resource/" + d["resources"][0]["id"]
-    #     )
-    #     assert "Camels in Photo" in response.body
-    #     assert "Date" in response.body
+    def test_choice_field_shows_list_if_multiple_options(self, app):
+        user = Sysadmin()
+        Dataset(
+            user=user,
+            type="test-schema",
+            name="with-multiple-choice-n",
+            personality=["friendly", "spits"],
+        )
 
-    # def test_choice_field_shows_labels(self, app):
-    #     user = Sysadmin()
-    #     Dataset(
-    #         user=user,
-    #         type="test-schema",
-    #         name="with-choice",
-    #         category="hybrid",
-    #     )
-    #     response = app.get("/dataset/with-choice")
-    #     assert "Hybrid Camel" in response.body
+        response = app.get("/dataset/with-multiple-choice-n")
 
-    # def test_notes_field_displayed(self, app):
-    #     user = Sysadmin()
-    #     Dataset(
-    #         user=user,
-    #         type="dataset",
-    #         name="plain-jane",
-    #         notes="# styled notes",
-    #     )
+        assert (
+            "<ul><li>Often friendly</li><li>Tends to spit</li></ul>"
+            in response.body
+        )
 
-    #     response = app.get("/dataset/plain-jane")
-    #     assert "<h1>styled notes" in response.body
+    def test_choice_field_does_not_show_list_if_one_options(self, app):
+        user = Sysadmin()
+        Dataset(
+            user=user,
+            type="test-schema",
+            name="with-multiple-choice-one",
+            personality=["friendly"],
+        )
 
-    # def test_choice_field_shows_list_if_multiple_options(self, app):
-    #     user = Sysadmin()
-    #     Dataset(
-    #         user=user,
-    #         type="test-schema",
-    #         name="with-multiple-choice-n",
-    #         personality=["friendly", "spits"],
-    #     )
+        response = app.get("/dataset/with-multiple-choice-one")
 
-    #     response = app.get("/dataset/with-multiple-choice-n")
+        assert "Often friendly" in response.body
+        assert "<ul><li>Often friendly</li></ul>" not in response.body
 
-    #     assert (
-    #         "<ul><li>Often friendly</li><li>Tends to spit</li></ul>"
-    #         in response.body
-    #     )
+    def test_json_field_displayed(self, app):
+        user = Sysadmin()
+        Dataset(
+            user=user,
+            type="test-schema",
+            name="plain-json",
+            a_json_field={"a": "1", "b": "2"},
+        )
+        response = app.get("/dataset/plain-json")
 
-    # def test_choice_field_does_not_show_list_if_one_options(self, app):
-    #     user = Sysadmin()
-    #     Dataset(
-    #         user=user,
-    #         type="test-schema",
-    #         name="with-multiple-choice-one",
-    #         personality=["friendly"],
-    #     )
+        if six.PY3:
+            expected = """{\n  "a": "1",\n  "b": "2"\n}"""
+        else:
+            expected = """{\n  "a": "1", \n  "b": "2"\n}"""
+        expected = expected.replace(
+            '"', "&#34;"
+        )  # Ask webhelpers
 
-    #     response = app.get("/dataset/with-multiple-choice-one")
-
-    #     assert "Often friendly" in response.body
-    #     assert "<ul><li>Often friendly</li></ul>" not in response.body
-
-    # def test_json_field_displayed(self, app):
-    #     user = Sysadmin()
-    #     Dataset(
-    #         user=user,
-    #         type="test-schema",
-    #         name="plain-json",
-    #         a_json_field={"a": "1", "b": "2"},
-    #     )
-    #     response = app.get("/dataset/plain-json")
-
-    #     if six.PY3:
-    #         expected = """{\n  "a": "1",\n  "b": "2"\n}"""
-    #     else:
-    #         expected = """{\n  "a": "1", \n  "b": "2"\n}"""
-    #     expected = expected.replace(
-    #         '"', "&#34;"
-    #     )  # Ask webhelpers
-
-    #     assert expected in response.body
-    #     assert "Example JSON" in response.body
+        assert expected in response.body
+        assert "Example JSON" in response.body
