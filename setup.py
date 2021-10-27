ed5338570524871380d302d7141af1a375fe0d06
sabinem <sabine.maennel@gmail.com>
2021-10-27 14:19:13 +0200

    set dependecy of pyyaml to version 5.4

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'pyyaml',
+        'pyyaml==5.4',
         'ckanapi',
         'ckantoolkit>=0.0.2',
         'pytz',
