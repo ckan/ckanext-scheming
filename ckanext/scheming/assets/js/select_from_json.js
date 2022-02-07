60579aa7216b039c8adbcb9b1b3ef608f34296dc
pthiemt <peggy.thiemt@tu-dresden.de>
2022-02-07 13:44:18 +0100

    save multiple values to same metric

--- ckanext/scheming/assets/js/select_from_json.js
+++ ckanext/scheming/assets/js/select_from_json.js
@@ -36,7 +36,6 @@ this.ckan.module('select-from-json', function (jQuery, _) {
 					'Accept': 'application/sparql-results+json'
 				}
 			});
-			console.log(url);
 			return response.json(); // parses JSON response into native JavaScript objects
 		},
 
@@ -52,7 +51,6 @@ this.ckan.module('select-from-json', function (jQuery, _) {
 
 		handleDataSparql: function (data = null) {
 			for (var i = 0; i < data.results.bindings.length; i++) {
-				// var label = data.results.bindings[i].subject.value.split("#")[1].replace(/([A-Z])/g, ' $1');
 				if (data.results.bindings[i].subject.value == this.options.selected) {
 					$(this.el[0]).append($('<option>', {
 						value: data.results.bindings[i].subject.value,
@@ -92,56 +90,84 @@ this.ckan.module('select-from-json', function (jQuery, _) {
 				// TODO:  handle json
 			}
 
-			var createBlock = function (url, values) {
+			var createBlock = function (label, url, value) {
 
 				// Create blocks default elements
-				var $block = $("<div>", { "class": "metrics-block", "id": "block-" + url.split("#")[1] });
-				$block.append("<label>" + metrics[url].label + '</label>');
-				$block.append('<a remove="' + url + '" class="btn btn-default btn-sm pull-right">Remove</a>');
-				$block.append('<p class="tiny-metrics-url">' + url + '</p>');
+				var block = $("<div>", { "class": "metrics-block"});
+				block.append("<label>" + label + '</label>');
+				block.append('<a remove="' + url + '" class="btn btn-default btn-sm pull-right">Remove</a>');
+				block.append('<p class="tiny-metrics-url">' + url + '</p>');
 
-				var $inputs = $('<div>', { "class": "input-group" });
-				for (var value in values) {
-					$inputs.append('<div class="input-group-row"><span class="input-group-addon">' + value + '</span><input name="' + url + '" type="text" value="' + values[value] + '" /></div>');
+				var inputs = $('<div>', { "class": "input-group" });
+				for (var subvalue in value) {
+					inputs.append('<div class="input-group-row"><span class="input-group-addon">' + subvalue + '</span><input name="' + url + '" type="text" value="' + value[subvalue] + '" /></div>');
 				}
 
 				// Set event handler to all inputs in current block
-				$inputs.children().on("input", function (e) {
-					metrics[$(e.target).attr("name")].values[$(e.currentTarget).find('span').text()] = $(e.target).val();
-					$(input).val(JSON.stringify(metrics));
-					console.log(metrics);
-				});
+				inputs.find("input").on("input", (e) => updateMetric());
+				
 				// Set event handler to remove button in current block
-				$block.find("a").on("click", function (e) {
-					var urlValue = $(e.target).attr("remove")
-					console.log(urlValue)
-					$("#block-" + urlValue.split("#")[1]).remove();
-					delete metrics[urlValue];
-					$(input).val(JSON.stringify(metrics));
+				block.find("a").on("click", (e) => {
+					e.target.closest('div.metrics-block').remove();
+					updateMetric();	
 				});
-				$block.append($inputs);
-				$(divMain).append($block);
+
+				block.append(inputs);
+				$(divMain).append(block);
+				
 			};
 
+			var updateMetric = function(){
+				metrics = new Object();
+				divMain.querySelectorAll("div[class='metrics-block']").forEach((node,index) => 
+				{
+					var metricUrl = $(node.querySelector("p[class='tiny-metrics-url'")).text()
+					if(typeof metrics[metricUrl] === 'undefined') {
+						metrics[metricUrl] = getEmptySubMetric($(node.querySelector("label")).text());	
+					}
+					else {
+						metrics[metricUrl].values.push(getEmptyValuesObj());
+					}
+					
+					node.querySelectorAll("span[class='input-group-addon'").forEach((subnode,subindex) => {
+						metrics[metricUrl].values[metrics[metricUrl].values.length-1][$(subnode).text()] = $(subnode.nextElementSibling).val();
+					});
+				});
+				$(input).val(JSON.stringify(metrics));
+			};
+			
 			var getEmptySubMetric = function (label) {
+				var result = {};
+				
+				result['label'] = label;
+				result['values'] = new Array();
+				result['values'].push(getEmptyValuesObj());
+
+				return result;
+			};
+			
+			var getEmptyValuesObj = function () {
 				var subLabels = options.sublabels.split("|");
 				var subValues = {};
-				var result = {};
 
 				for (var i = 0; i < subLabels.length; i++) {
 					subValues[subLabels[i]] = "";
 				}
-				result['label'] = label;
-				result['values'] = subValues;
-
-				return result;
+				return subValues;
 			};
-
 
 			if (this.el.data('metrics_data')) {
 				metrics = this.el.data('metrics_data');
-				for (var metric in metrics) {
-					createBlock(metric, metrics[metric].values);
+				for( var metric in metrics ) {
+					// Backwards compatibility for previous datasets where values not saved as array
+					if(Array.isArray(metrics[metric].values)) {
+						for( var value in metrics[metric].values){
+							createBlock(metrics[metric].label, metric, metrics[metric].values[value]);
+						}
+					}
+					else {
+						createBlock(metrics[metric].label, metric, metrics[metric].values);
+					}
 				}
 			}
 
@@ -149,19 +175,8 @@ this.ckan.module('select-from-json', function (jQuery, _) {
 				e.preventDefault();
 
 				if ($(sel).val() != "") {
-					// createBlock($(sel).children("option").filter(":selected").text(), $(sel).val());
-
-					metrics[$(sel).val()] = getEmptySubMetric(sel.options[sel.selectedIndex].text);
-
-					console.log(sel.options[sel.selectedIndex].text);
-					// console.log(metrics)
-					createBlock($(sel).val(), metrics[$(sel).val()].values);
-					//metrics.name = $(sel).children("option").filter(":selected").text()
-					//metrics.value = $(sel).val()
-					//metrics[$(sel).val()] = {"value1":"blubb","value2":"blubbi","value3":"blubb","value4":"blubbi"};
-					//console.log($("#field-exfield_12-btn").text());
-					//localStorage.setItem("metricsJSON", JSON.stringify(metrics));
-					$(input).val(JSON.stringify(metrics));
+					createBlock(sel.options[sel.selectedIndex].text, $(sel).val(), getEmptyValuesObj());
+					updateMetric();
 				}
 			});
 
