474fbe1b29a1fce03c2da68f129c625b5860a185
amercader <amercadero@gmail.com>
2022-04-01 12:30:33 +0200

    Replace builtin unicode function as validator

--- ckanext/scheming/validation.py
+++ ckanext/scheming/validation.py
@@ -408,8 +408,8 @@ def validators_from_string(s, field, schema):
     """
     convert a schema validators string to a list of validators
 
-    e.g. "if_empty_same_as(name) unicode" becomes:
-    [if_empty_same_as("name"), unicode]
+    e.g. "if_empty_same_as(name) unicode_safe" becomes:
+    [if_empty_same_as("name"), unicode_safe]
     """
     out = []
     parts = s.split()
